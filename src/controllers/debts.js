9a34758cc7260aade20f25cf8533d81fb910e09f
Mfrodriguezp <mildriz06@gmail.com>
2022-02-14 22:22:25 -0500

    The routes put y delete for customers has been created

--- src/controllers/debts.js
+++ src/controllers/debts.js
@@ -25,6 +25,67 @@ var controller = {
             }
         });
     },
+    setCustomer: function (req, res) {
+        let params = req.body;
+        if (params.customerName) {
+            mysqlConnection.query(`INSERT INTO customers (customerName,phoneNumber) VALUES ("${params.customerName}","${params.phoneNumber}")`, (err, result, fiels) => {
+                if (!err) {
+                    res.status(200).send({
+                        message: "El cliente se ha creado correctamente"
+                    });
+                } else {
+                    res.status(500).send({
+                        message: `se ha producido un error en el servidor: ${err}`
+                    });
+                }
+            });
+        }
+    },
+    editCustomer: function (req, res) {
+        let customerId= req.params.id;
+        let params = req.body;
+        mysqlConnection.query(`UPDATE customers SET customerName = "${params.customerName}", phoneNumber = "${params.phoneNumber}" WHERE idcustomers = ${customerId}`,(err, results, fiels)=>{
+            if(!err){
+                if(results.affectedRows ===0){
+                    return res.status(404).send({
+                        message: "The user request no exist"
+                    });
+                }else{
+                    if(results.affectedRows >= 1){
+                        return res.status(200).send({
+                            message: "The user has been modified correctly"
+                        });
+                    }
+                }
+            }else{
+                return res.status(500).send({
+                    message: `The following error has been generated: ${err}`
+                });
+            }
+        });
+    },
+    deleteCustomer:function(req,res){
+        let customerId= req.params.id;
+        mysqlConnection.query("DELETE FROM customers WHERE idcustomers = ?",[customerId],(err, results, fiels)=>{
+            if(!err){
+                if(results.affectedRows ===0){
+                    return res.status(404).send({
+                        message: "The user request no exist"
+                    });
+                }else{
+                    if(results.affectedRows >= 1){
+                        return res.status(200).send({
+                            message: "The user has been deleted correctly"
+                        });
+                    }
+                }
+            }else{
+                return res.status(500).send({
+                    message: `The following error has been generated: ${err}`
+                });
+            }
+        });
+    },
     getDebts: function (req, res) {
         mysqlConnection.query(`select iddebts,customers.idcustomers, customers.customerName, debtValue, debtState, dateDebt, paymentDate 
                                 from debts inner JOIN customers 
@@ -40,25 +101,8 @@ var controller = {
                     });
                 }
             });
-    },
-    setCustomer : function(req,res){
-        let params = req.body;
-        if(params.customerName){
-            mysqlConnection.query(`INSERT INTO customers (customerName,phoneNumber) VALUES ("${params.customerName}","${params.phoneNumber}")`,(err,result,fiels)=>{
-                if(!err){
-                    res.status(200).send({
-                        message: "El cliente se ha creado correctamente"
-                    });
-                }else{
-                    res.status(500).send({
-                        message: `se ha producido un error en el servidor: ${err}`
-                    });
-                }
-            });
-        }
     }
 
 };
-
 
 module.exports = controller;
--- src/routes/debts.js
+++ src/routes/debts.js
@@ -6,7 +6,9 @@ const router = express.Router();
 //Routes
 router.get('/getCustomers',debtController.getCustomers);
 router.get('/getCustomer/:id',debtController.getCustomer);
-router.get('/getDebts',debtController.getDebts);
 router.post('/setCustomer',debtController.setCustomer);
+router.put('/editCustomer/:id',debtController.editCustomer);
+router.delete('/deleteCustomer/:id',debtController.deleteCustomer);
+router.get('/getDebts',debtController.getDebts);
 
 module.exports = router;
