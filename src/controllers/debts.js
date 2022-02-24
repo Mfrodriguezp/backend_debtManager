28d85c71392a491f369d5b07e9a9636aa0484b78
Mfrodriguezp <mildriz06@gmail.com>
2022-02-23 22:11:41 -0500

    The router getDebt, get Debts, setDebt and editDebt has been created

--- src/controllers/debts.js
+++ src/controllers/debts.js
@@ -42,44 +42,44 @@ var controller = {
         }
     },
     editCustomer: function (req, res) {
-        let customerId= req.params.id;
+        let customerId = req.params.id;
         let params = req.body;
-        mysqlConnection.query(`UPDATE customers SET customerName = "${params.customerName}", phoneNumber = "${params.phoneNumber}" WHERE idcustomers = ${customerId}`,(err, results, fiels)=>{
-            if(!err){
-                if(results.affectedRows ===0){
+        mysqlConnection.query(`UPDATE customers SET customerName = "${params.customerName}", phoneNumber = "${params.phoneNumber}" WHERE idcustomers = ${customerId}`, (err, results, fiels) => {
+            if (!err) {
+                if (results.affectedRows === 0) {
                     return res.status(404).send({
                         message: "The user request no exist"
                     });
-                }else{
-                    if(results.affectedRows >= 1){
+                } else {
+                    if (results.affectedRows >= 1) {
                         return res.status(200).send({
                             message: "The user has been modified correctly"
                         });
                     }
                 }
-            }else{
+            } else {
                 return res.status(500).send({
                     message: `The following error has been generated: ${err}`
                 });
             }
         });
     },
-    deleteCustomer:function(req,res){
-        let customerId= req.params.id;
-        mysqlConnection.query("DELETE FROM customers WHERE idcustomers = ?",[customerId],(err, results, fiels)=>{
-            if(!err){
-                if(results.affectedRows ===0){
+    deleteCustomer: function (req, res) {
+        let customerId = req.params.id;
+        mysqlConnection.query("DELETE FROM customers WHERE idcustomers = ?", [customerId], (err, results, fiels) => {
+            if (!err) {
+                if (results.affectedRows === 0) {
                     return res.status(404).send({
                         message: "The user request no exist"
                     });
-                }else{
-                    if(results.affectedRows >= 1){
+                } else {
+                    if (results.affectedRows >= 1) {
                         return res.status(200).send({
                             message: "The user has been deleted correctly"
                         });
                     }
                 }
-            }else{
+            } else {
                 return res.status(500).send({
                     message: `The following error has been generated: ${err}`
                 });
@@ -92,17 +92,100 @@ var controller = {
                                 on debts.customers_idcustomers = customers.idcustomers;`,
             (err, rows, fiels) => {
                 if (!err) {
-                    res.status(200).json({
-                        debts: rows
-                    });
+                    if (rows.length = 'undefined') {
+                        return res.status(404).send({
+                            message: "debts table is empty"
+                        });
+                    } else {
+                        return res.status(200).send({
+                            debts: rows
+                        });
+                    }
                 } else {
-                    res.status(404).send({
+                    return res.status(404).send({
                         message: `Se ha generado el siguiente error: ${err}`
                     });
                 }
             });
+    },
+    getDebt: function (req, res) {
+        let debtId = req.params.id;
+        mysqlConnection.query(`select iddebts,customers.idcustomers, customers.customerName, debtValue, debtState, dateDebt, paymentDate 
+            from debts inner JOIN customers 
+            on debts.customers_idcustomers = customers.idcustomers WHERE iddebts =?;`, [debtId],
+            (err, rows, fiels) => {
+                if (!err) {
+                    if (rows.length) {
+                        return res.status(200).send({
+                            debt: rows
+                        });
+                    } else {
+                        return res.status(404).send({
+                            message: "The id requested not exist"
+                        });
+                    }
+                } else {
+                    return res.status(500).send({
+                        message: `The following error has been generated: ${err}`
+                    });
+                }
+            });
+    },
+    setDebt: function (req, res) {
+        let params = req.body;
+        mysqlConnection.query('SELECT idcustomers,customerName FROM customers WHERE customerName = ?', [params.customerName], (err, rows, fiels) => {
+            if (!err) {
+                let data = rows.map((field) => {
+                    return field.idcustomers;
+                });
+                if (data.length >= 0) {
+                    mysqlConnection.query(`insert into debts (customers_idcustomers,debtValue,dateDebt,debtState) 
+                                            VALUES (?,?,DATE_FORMAT(now(),'%Y-%m-%d'),'Debe')`, [data[0], params.debtValue],
+                        (err, results, fiels) => {
+                            if (!err) {
+                                res.status(200).send({
+                                    message: `Has creadted ${results.affectedRows} rows`
+                                });
+                            } else {
+                                res.status(500).send({
+                                    message: `The following error has been generated: ${err}`
+                                });
+                            }
+                        });
+                } else {
+                    res.status(404).send({
+                        message: "El cliente no se encuentra creado"
+                    });
+                }
+            } else {
+                res.status(500).send({
+                    message: `The following error has been generated: ${err}`
+                });
+            }
+        });
+    },
+    editDebt: function (req, res) {
+        let debtId = req.params.id;
+        let params = req.body;
+        mysqlConnection.query(`UPDATE debts SET debtValue = ?, debtState = ?, paymentDate = DATE_FORMAT(now(),'%Y-%m-%d') 
+                                WHERE  iddebts = ?`, [params.debtPayment, params.debtState, debtId], (err, results, fiels) => {
+            if (!err) {
+                if (results.affectedRows === 0) {
+                    return res.status(404).send({
+                        message: "The debt request no exist"
+                    });
+                } else {
+                    return res.status(200).send({
+                        message: "The debt has been modified correctly"
+                    });
+                }
+            } else {
+                return res.status(500).send({
+                    message: `The following error has been generated: ${err}`
+                });
+            }
+        });
     }
-
 };
 
 module.exports = controller;
--- src/routes/debts.js
+++ src/routes/debts.js
@@ -10,5 +10,8 @@ router.post('/setCustomer',debtController.setCustomer);
 router.put('/editCustomer/:id',debtController.editCustomer);
 router.delete('/deleteCustomer/:id',debtController.deleteCustomer);
 router.get('/getDebts',debtController.getDebts);
+router.get('/getDebt/:id',debtController.getDebt);
+router.post('/setDebt',debtController.setDebt);
+router.put('/editDebt/:id',debtController.editDebt);
 
 module.exports = router;
