ee30e0e5f65c9fcf21c8207b5bc773b632f7239b
Mfrodriguezp <mildriz06@gmail.com>
2022-06-22 10:33:28 -0500

    it has been removed packaget unnecessary

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const cors = require('cors');
 
 const cookieParser = require('cookie-parser');
--- src/database.js
+++ src/database.js
@@ -1,5 +1,4 @@
 const mysql = require('mysql');
-const dotenv = require('dotenv');
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
