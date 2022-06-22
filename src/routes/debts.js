ee94ca19e5f7203d5186c4f3a5bd53d3c949a5dd
Mfrodriguezp <mildriz06@gmail.com>
2022-06-22 17:07:46 -0500

    it has been created middleware check Role in process admin

--- src/routes/debts.js
+++ src/routes/debts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const debtController = require('../controllers/debts');
 const checkJwt = require('../middleware/checkJwt');
+const checkRole = require('../middleware/checkRole');
 
 const router = express.Router();
 
@@ -18,7 +19,7 @@ router.put('/editDebt/:id',debtController.editDebt);
 router.delete('/deleteDebt/:id',debtController.deleteDebt);
 router.get('/getTotalDebts',debtController.getTotalDebts);
 //admin only
-router.post('/registrerNewUser',checkJwt.checkJwt,debtController.registrerNewUser);
+router.post('/registrerNewUser',[checkJwt.checkJwt,checkRole(['Admin'])],debtController.registrerNewUser);
 router.post('/login',debtController.login);
 router.get('/logout',debtController.logout);
 
