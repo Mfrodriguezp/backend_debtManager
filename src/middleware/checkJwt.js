3dd6a4f91f826281d82d1742b8929a9429f71bdb
Mfrodriguezp <mildriz06@gmail.com>
2022-06-22 17:21:09 -0500

    assignation jwtPayload to local variables

--- src/middleware/checkJwt.js
+++ src/middleware/checkJwt.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 function checkJwt(req, res, next) {
-
+    //verify header auth in request
     const token = req.headers['auth'];
     let jwtPayload;
-
     try {
         jwtPayload = jwt.verify(token, process.env.JWT_SECRET);
+        res.locals.jwtPayload = jwtPayload;
     } catch (e) {
         return res.status(401).send({
             message: "Not Authorized"
